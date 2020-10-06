c5e007584de56ec0ac49736d427b46f61bce45d2
Dean Lawrence <djlawren@umich.edu>
2020-10-05 23:16:39 -0400

    Smaller outputs on regressors

--- eyelib/GazeEstimation.py
+++ eyelib/GazeEstimation.py
@@ -9,7 +9,7 @@ import numpy as np
 import copy
 
 class GazeEstimation():
-    def __init__(self, x_estimator, y_estimator):
+    def __init__(self, x_estimator, y_estimator, width, height):
         
         self.data = {
             "x_predictors": [],
@@ -20,6 +20,9 @@ class GazeEstimation():
 
         self.x_estimator = x_estimator
         self.y_estimator = y_estimator
+
+        self.width = width
+        self.height = height
 
         self._trained = False
 
@@ -41,10 +44,10 @@ class GazeEstimation():
         y_features = predictor[1:-1:2]
 
         self.data["x_predictors"].append(x_features)
-        self.data["x_labels"].append(label[0])
+        self.data["x_labels"].append(label[0] / self.width)
 
         self.data["y_predictors"].append(y_features)
-        self.data["y_labels"].append(label[1])
+        self.data["y_labels"].append(label[1] / self.height)
 
     def train(self):
         x_predictors = np.array(self.data["x_predictors"])
@@ -65,8 +68,8 @@ class GazeEstimation():
         x_features = np.array(predictor[0:-1:2]).reshape(1, -1)
         y_features = np.array(predictor[1:-1:2]).reshape(1, -1)
 
-        return (int(self.x_estimator.predict(x_features)),
-                int(self.y_estimator.predict(y_features)))
+        return (int(self.x_estimator.predict(x_features) * self.width),
+                int(self.y_estimator.predict(y_features) * self.height))
     
     def is_trained(self):
         return self._trained
--- eyelib/GazeEstimationThread.py
+++ eyelib/GazeEstimationThread.py
@@ -14,19 +14,26 @@ from . import FeatureExtraction
 class GazeEstimationThread():
     def __init__(self, x_estimator, y_estimator, face_cascade_path, eye_cascade_path, shape_predictor_path, width, height):
         
-        self.width = width
-        self.height = height
+        self._width = width
+        self._height = height
 
-        self.gaze_estimator = GazeEstimation(x_estimator, y_estimator)
-        self.feature_extractor = FeatureExtraction(face_cascade_path, eye_cascade_path, shape_predictor_path)
+        self._gaze_estimator = GazeEstimation(x_estimator, y_estimator, self._width, self._height)
+        self._feature_extractor = FeatureExtraction(face_cascade_path, eye_cascade_path, shape_predictor_path)
 
-        self.gaze_queue = queue.Queue(0)
+        self._gaze_queue = queue.Queue(0)
 
-        self.thread = threading.Thread(target=self.run)
-        self.thread.daemon = True
-        self.thread.start()
+        self._thread = threading.Thread(target=self.run)
+        self._thread.daemon = True
+        self._thread.start()
 
     def run(self):
         while True:
-            self.feature_extractor.update_feature_state()
-            self.gaze_queue.put(self.gaze_estimator.predict(self.feature_extractor.get_state_as_vector()))
+            self._feature_extractor.update_feature_state()
+            self._gaze_queue.put(self._gaze_estimator.predict(self._feature_extractor.get_state_as_vector()))
+    
+    def get(self):
+        if self._gaze_queue.qsize() == 0:
+            return None
+        else:
+            return self._gaze_queue.get()
+
